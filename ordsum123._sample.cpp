f4c1f661d1915622cf21ad11c781b251f487d9e2
suman kumar <sumankumar667@gmail.com>
2016-06-16 01:53:35 -0400

    http://www.spoj.com/problems/ORDSUM23/
    this will true for only even value of n i.e.(n%2==0) and n<=45 only so need to work on it more

---
+++ ordsum123._sample.cpp
@@ -0,0 +1,33 @@
+//ordsum23 problem partial checking 
+//sample checking whether it is working or not 
+#include<iostream>
+using namespace std;
+long long int fact(long long int n)
+{
+	if(n==0)
+	    return 1;
+	if(n!=1)
+	    return n*fact(n-1);
+}
+int main()
+{
+	long long int n;
+	cin>>n;
+	long long int no_of_two=n/2;
+	if(no_of_two<=2)
+	    cout<<"1"<<endl;
+	else
+	{
+		long long int total=0;
+		long long int no_of_three=0;
+		while(no_of_two>=0)
+		{
+			total+=(fact(no_of_two+no_of_three)/(fact(no_of_two)*fact(no_of_three)));
+			no_of_two-=3;
+			no_of_three+=2;
+		}
+		cout<<total<<endl;
+	}
+	
+	return 0;
+}
