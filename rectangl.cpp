b7c3dcf062475773e0afe6d214a4dbf83c970e81
suman kumar <sumankumar667@gmail.com>
2018-01-05 15:24:33 +0530

    codechef january long challenge problem

---
+++ rectangl.cpp
@@ -0,0 +1,26 @@
+/*
+ codechef long challenge question
+code : rectagl
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(a==b && c==d)
+            cout<<"YES"<<endl;
+        else if(a==c && b==d)
+            cout<<"YES"<<endl;
+        else if(a==d && b==c)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
